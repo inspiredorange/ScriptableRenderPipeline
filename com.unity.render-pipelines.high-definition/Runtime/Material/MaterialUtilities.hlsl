65e77425c9b073c1ca1264a08287def0e941503c
Evgenii Golubev <evgenii@unity3d.com>
2019-01-22 10:37:13 +0100

    Move flip normal handling to the resolve point

--- com.unity.render-pipelines.high-definition/Runtime/Material/MaterialUtilities.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Material/MaterialUtilities.hlsl
@@ -8,15 +8,7 @@ void ApplyDoubleSidedFlipOrMirror(inout FragInputs input)
     // For the 'Flip' mode, we should not modify the tangent and the bitangent (which correspond
     // to the surface derivatives), and instead modify (invert) the displacements.
     input.worldToTangent[2] = flipSign * input.worldToTangent[2]; // normal
-
-#ifndef SURFACE_GRADIENT
-    // Do the stupid thing and just flip the TB. Conceptually wrong, but it works.
-    float2 flipSigns = input.isFrontFace ? float2(1.0, 1.0) : _DoubleSidedConstants.xy;
-    input.worldToTangent[0] = flipSigns.x * input.worldToTangent[0]; // tangent
-    input.worldToTangent[1] = flipSigns.y * input.worldToTangent[1]; // bitangent
-#endif // SURFACE_GRADIENT
-
-#endif // _DOUBLESIDED_ON
+#endif
 }
 
 // This function convert the tangent space normal/tangent to world space and orthonormalize it + apply a correction of the normal if it is not pointing towards the near plane
@@ -31,7 +23,16 @@ void GetNormalWS(FragInputs input, float3 normalTS, out float3 normalWS)
 #endif // _DOUBLESIDED_ON
 
     normalWS = SurfaceGradientResolveNormal(input.worldToTangent[2], normalTS);
-#else
+
+#else // SURFACE_GRADIENT
+
+#ifdef _DOUBLESIDED_ON
+    // Just flip the TB in the 'flip normal' mode. Conceptually wrong, but it works.
+    float2 flipSigns = input.isFrontFace ? float2(1.0, 1.0) : _DoubleSidedConstants.xy;
+    input.worldToTangent[0] = flipSigns.x * input.worldToTangent[0]; // tangent
+    input.worldToTangent[1] = flipSigns.y * input.worldToTangent[1]; // bitangent
+#endif // _DOUBLESIDED_ON
+
     // We need to normalize as we use mikkt tangent space and this is expected (tangent space is not normalize)
     normalWS = normalize(TransformTangentToWorld(normalTS, input.worldToTangent));
 #endif
